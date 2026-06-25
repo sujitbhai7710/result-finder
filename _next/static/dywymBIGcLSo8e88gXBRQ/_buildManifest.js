9c86ccb5dc8ca111f940ef3b8f31c736fc5c5489
Result Finder Deploy <deploy@result-finder.local>
2026-06-25 13:42:22 +0000

    Initial: 756 students (375 Sem IV + 381 Sem V) with semester filter

---
+++ _next/static/dywymBIGcLSo8e88gXBRQ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/result-finder//_next/:path+",
+        "destination": "/result-finder/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
